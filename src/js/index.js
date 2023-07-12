f7a902cbda0766c7bed4c3c4093835cc34b0c74e
Luiz Ricardo <37805398+luiz1910@users.noreply.github.com>
2023-07-11 22:10:20 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+const personagens = document.querySelectorAll('.personagem');
+
+
+//adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        
+        if(window.innerWidth < 450){
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+
+        //verificar se já existe um personagem selecionado, se sim, remover a seleção dele
+        removerSelecaoDoPersonagem();
+
+        personagem.classList.add('selecionado');
+
+        //pegar o elemento do personagem grande pra adicionar as informações nele
+        alterarImagemPersonagemSelecionado(personagem);
+
+        //alterar o nome do personagem grande
+        alterarNomePersonagemSelecionado(personagem);
+
+        //alterar a descrição do personagem
+        alterarDescricaoPersonagemSelecionado(personagem);
+    })
+})
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+    //alterar a imagem do personagem grande
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
